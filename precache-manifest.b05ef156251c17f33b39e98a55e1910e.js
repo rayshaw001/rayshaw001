b752e0fa05d56ffffec11b0680b1ed135baf1066
xiaorui <rxiao@hpe.com>
2018-10-10 13:41:48 +0800

    update

---
+++ precache-manifest.b05ef156251c17f33b39e98a55e1910e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdd5d4980fe64644f6f4",
+    "url": "/static/js/main.cdd5d498.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "cdd5d4980fe64644f6f4",
+    "url": "/static/css/main.c7898583.chunk.css"
+  },
+  {
+    "revision": "380a1405da3eb8e54b7688bbf39e9d3d",
+    "url": "/index.html"
+  }
+];
