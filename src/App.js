5caf6430cc5258db710aaf3044a210a95a87aab0
xiaorui <rxiao@hpe.com>
2018-10-10 12:22:05 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="./moreAboutMe.html"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            More About Me
           </a>
         </header>
       </div>
