1e98f49cf8622f4a38ebb54fba6caa4b41a361db
Ray Xiao <xiaorui1@xiaomi.com>
2023-02-14 23:01:15 +0800

    fix wave

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -17,10 +17,10 @@ class AboutMe extends Component {
                     <h2 className="h">Leetcode <a className="a" href="https://leetcode.cn/rayshaw001/">rayshaw001</a></h2>
                     <h2 className="h">Netease Cloud Music <a className="a" href="https://music.163.com/#/user/home?id=74132453">RayShaw</a></h2>
                     </div>
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="800" width="auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="800" width="auto">
                         <polyline points="0,400 40,100 80,600 120,155 160,477 200,55 240,555 280,100 320,666 360,234 400,555 440,277 480,555 520,123 560,722 600,100 640,600 680,155 720,477 760,55 800,555 840,300 880,700 920,321 960,567 1000,77 1040,585 1080,355 1120,800 1160,100 1200,600 1240,155 1280,477 1320,55 1360,555 1400,400 1440,100 1480,600 1520,155 1560,477 1600,55 1640,555 1680,120 1720,700 1760,100 1800,800 1840,150 1880,650 1920,300"
-                            style="fill:transparent;stroke:#01A982;stroke-width:3;fill-rule:evenodd;repeat-x:true" />
-                    </svg> */}
+                            className='polyLine' />
+                    </svg>
                 </div>   
                 <canvas></canvas>
                 <script type="text/javascript" src="./js/three.min.js"></script>
